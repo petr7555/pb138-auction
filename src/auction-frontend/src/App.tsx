825e0f67e9a78c227991aa36a98d1e0eace2ee4a
Jakub Suslik <xsuslik@fi.muni.cz>
2020-05-17 20:54:18 +0200

    styled login

--- src/auction-frontend/src/App.tsx
+++ src/auction-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
 import 'antd/dist/antd.css';
+import './styles.css';
 import {Login} from "./components/Login";
 import {useStores} from "./hooks/use-stores";
 import {observer} from "mobx-react-lite";
