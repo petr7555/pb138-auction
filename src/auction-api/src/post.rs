a7a13580c63046b4eb04f70cc51c03767678a8f7
Otakar Hirs <ota@localhost.localdomain>
2020-05-25 11:37:32 +0200

    fix login second time as different user

--- src/auction-api/src/post.rs
+++ src/auction-api/src/post.rs
@@ -78,9 +78,8 @@ pub async fn login_user(
     if let Ok(user_res) = res {
         if let Some(user_res) = user_res {
             if user.password() == user_res.password() {
-                if let None = id.identity() {
-                    id.remember(user_res.id.to_string());
-                }
+                id.forget();
+                id.remember(user_res.id.to_string());
                 return Ok(HttpResponse::Ok().json(user_res))
             }
         }
