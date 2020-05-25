8056b16e9fe05df2cc0557b465e8045d98113608
Otakar Hirs <ota@localhost.localdomain>
2020-05-25 12:36:06 +0200

    code cleanup

--- src/auction-api/src/post.rs
+++ src/auction-api/src/post.rs
@@ -78,7 +78,6 @@ pub async fn login_user(
     if let Ok(user_res) = res {
         if let Some(user_res) = user_res {
             if user.password() == user_res.password() {
-                id.forget();
                 id.remember(user_res.id.to_string());
                 return Ok(HttpResponse::Ok().json(user_res))
             }
@@ -89,7 +88,7 @@ pub async fn login_user(
 }
 
 fn identity_check(id: Identity, user_id: i64) -> Result<(), HttpResponse> {
-    if let None = id.identity() {
+    if id.identity().is_none() {
         return Err(HttpResponse::Unauthorized().json(ErrorResponse::from("Authorization required".to_owned())))
     }
     if id.identity().unwrap() != user_id.to_string() {
